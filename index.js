39c9d95b432b6099f8b8e5f7aa86b862a1328691
leandredb75 <leandredibipro@gmail.com>
2021-02-02 10:54:51 +0100

    Add function hello

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ const app = express()
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+
+function helloLeandre() {
+ console.log('Hello Léandre')
+}
+helloLeandre()
  
 app.listen(3001)
